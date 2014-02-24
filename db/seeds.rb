a2fada9459083338ab608f3f0910ce3c007b1555
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-02-24 17:49:54 -0500

    Added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,14 +32,25 @@ rand(4..10).times do
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
-
-    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
-
-# comments
+    # comments
     rand(3..7).times do
       p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
     end
+
+    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics
+    
+  end
+end
+
+post_count = Post.count
+User.all.each do |user|
+  rand(30..50).times do
+    p = Post.find(rand(1..post_count))
+    c = user.comments.create(
+      body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+      post: p)
+    c.update_attribute(:created_at, Time.now - rand(600..31536000))
   end
 end
 # This will eliminate the burden of creating your own user every time you refresh the database. 
