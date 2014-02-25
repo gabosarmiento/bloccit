13e8d84801818856d42b2ed6a98415f5a530495b
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-02-25 00:56:22 -0500

    Destroyed topics, posts and comments.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Bloccit::Application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index] do
-         resource :comments, only: [:create]
+      resources :comments, only: [:create, :destroy]
     end
   end
 
