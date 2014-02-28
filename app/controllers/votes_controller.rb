af374275c8f395a7f120df795cda3b2bee4a9260
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-02-28 12:24:42 -0500

    changed votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -22,26 +22,15 @@ class VotesController < ApplicationController
     # Look for an existing vote by this person so we don't create multiple
     @vote = @post.votes.where(user_id: current_user.id).first
   end
-  #Original update_vote method
-  #def update_vote(new_value)
-  #  if @vote # if it exists, update it
-  #    @vote.update_attribute(:value, new_value)
-  #  else # create it
-  #    @vote = current_user.votes.create(value: new_value, post: @post)
-  # end
-  #end
 
-  #if the user regrets his decision it shouldn't take into account his vote.
-  # My analysis was made base on the fact that @vote.vale is either empty, 1 or -1.
   def update_vote(new_value)
-    if @vote && @vote.value == new_value # if it exists, and is equal do nothing
-      @vote.update_attribute(:value, 0)
-    elsif @vote && @vote.value != new_value # if it exists, and is equal do nothing
+    if @vote && @vote.value == new_value # if it exists and is equal delete it to go back to undo state
+      @vote.destroy
+    elsif @vote && @vote.value != new_value # if it exists and is different update it (this is how reddit works)
       @vote.update_attribute(:value, new_value)
-    elsif #create it just as it was in the original method
+    elsif # create it
       @vote = current_user.votes.create(value: new_value, post: @post) 
     end
   end
   
-
 end
